f6e05d323db46e60f57b5cd85c0404af1525b154
Khalil Mohammad <khalilmohammadmirza@gmail.com>
2018-10-13 17:34:49 +0500

    Added MimeType Service

--- CustomGoogleDrive/Startup.cs
+++ CustomGoogleDrive/Startup.cs
@@ -10,6 +10,7 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using CustomGoogleDrive.Data;
+using CustomGoogleDrive.Services;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.AspNetCore.Authentication;
@@ -77,6 +78,8 @@ namespace CustomGoogleDrive
                 });
 
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+
+            services.AddScoped<IGoogleDriveService, GoogleDriveService>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
